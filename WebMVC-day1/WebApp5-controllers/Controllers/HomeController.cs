2c1acbc6be8101353e5a0dc07f8a72144057fb9f
Andres <a0z9@rambler.ru>
2024-10-04 11:01:54 +0300

    Controller params

--- WebMVC-day1/WebApp5-controllers/Controllers/HomeController.cs
+++ WebMVC-day1/WebApp5-controllers/Controllers/HomeController.cs
@@ -9,10 +9,17 @@ namespace WebApp5_controllers.Controllers
         [ActionName("Index")]
         public IActionResult Test()
         {
+            
             Response.ContentType = "text/html;charset=utf-8";
+
+            Response.Headers.Add("Institute", "IKIT");
 
             StringBuilder s = new StringBuilder();
             foreach(var h in Request.Headers)
+                s.Append($"{h.Key} -- {h.Value}<br/>");
+           
+            s.Append("<hr/>");
+            foreach (var h in Response.Headers)
                 s.Append($"{h.Key} -- {h.Value}<br/>");
 
             return Content("<h2>MVC controller/Обучение ИКИТ</h2><hr/>" + 
@@ -21,9 +28,82 @@ namespace WebApp5_controllers.Controllers
             
         }
 
+        [HttpGet]
+        public IActionResult Student1(string name, string department="ПромАвто")
+        {
+
+            Response.ContentType = "text/html;charset=utf-8";
+            Response.Headers.Append("Institute", "IKIT");
+
+            StringBuilder s = new StringBuilder();
+            //foreach (var h in Request.Headers)
+            //    s.Append($"{h.Key} -- {h.Value}<br/>");
+            s.Append($"Имя студента: {name}<br/>");
+            s.Append($"Факультет: {department}<br/>");
+            s.Append("<hr/>");
+            
+            //foreach (var h in Response.Headers)
+            //    s.Append($"{h.Key} -- {h.Value}<br/>");
+
+            return Content("<h2>MVC controller/Обучение ИКИТ</h2><hr/>" +
+                s.ToString()
+                );
+
+        }
+
+        [HttpGet]
+        public IActionResult Student2()
+        {
+            string name=Request.Query["name"];
+            string department = Request.Query["department"];
+            if (String.IsNullOrEmpty(department)) department = "ПромАвто";
+
+            Response.ContentType = "text/html;charset=utf-8";
+            Response.Headers.Append("Institute", "IKIT");
+
+            StringBuilder s = new StringBuilder();
+           
+            s.Append($"Имя студента: {name}<br/>");
+            s.Append($"Факультет: {department}<br/>");
+            s.Append("<hr/>");
+
+           
+            return Content("<h2>MVC controller/Обучение ИКИТ</h2><hr/>" +
+                s.ToString()
+                );
+
+        }
+
+        [HttpGet]
+        public IActionResult Student3(Student st)
+        {
+            
+            Response.ContentType = "text/html;charset=utf-8";
+            Response.Headers.Append("Institute", "IKIT");
+
+            StringBuilder s = new StringBuilder();
+
+            s.Append($"Имя студента: {st.Name}<br/>");
+            s.Append($"Факультет: {st.Department}<br/>");
+            s.Append("<hr/>");
+
+
+            return Content("<h2>MVC controller/Обучение ИКИТ</h2><hr/>" +
+                s.ToString()
+                );
+
+        }
+
+
+       
 
 
 
 
+    }
+   public class Student
+    {
+        public string? Name { get; set; }
+        public string? Department { get; set; } = "ПромАвто";
     }
 }
