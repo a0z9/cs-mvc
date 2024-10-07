7804c1b7f0201043337c143399a7f4cd83089822
Andres <a0z9@rambler.ru>
2024-10-07 16:57:09 +0300

    models

--- WebMVC-day1/WebApp6-mvc/Models/StudentModel.cs
+++ WebMVC-day1/WebApp6-mvc/Models/StudentModel.cs
@@ -1,8 +1,34 @@
-﻿namespace WebApp6_mvc.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace WebApp6_mvc.Models
 {
     public record class StudentModel(string Id, string Password, string Grade="9", 
         string Department = "Phys");
 
+
+    public class Student {
+
+        [BindRequired]
+        public int Id { get; set; } //email
+
+        [BindRequired]
+        public string Name { get; set; }
+
+        [BindRequired]
+        public string Sname { get; set; }
+
+        [BindNever]
+        public string? Department { get; set; }
+        [BindNever]
+        public DateTime ExamDate {  get; set; }
+        [BindNever]
+        public Guid? InternalId { get; set; }
+        [BindNever]
+        public double? Grade { get; set; }
+        //...
+
+
+    }
 
     public static class Students
     { 
