4b257faf5dc6be8a747bee0f72d7846e8fc6555a
Andres <a0z9@rambler.ru>
2024-10-08 07:22:24 +0300

    binder - working

--- WebMVC-day1/WebApp7-models/Program.cs
+++ WebMVC-day1/WebApp7-models/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 //builder.Services.AddControllersWithViews();
 builder.Services.AddControllersWithViews(
-    opt => opt.ModelBinderProviders.Add(new RolePeopleModelBinderProvider())
+    opt => opt.ModelBinderProviders.Insert(0,new RolePeopleModelBinderProvider())
     );
 
 
